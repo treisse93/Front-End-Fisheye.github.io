361b9e9175f13cdd73dae79da4cda6333716b775
treisse93 <misstresse.slt@hotmail.fr>
2023-10-10 11:35:25 +0200

    fichiers css mise à jour

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,9 +1,8 @@
-
 // import de la fonction photographerFact() du fichier photographerFact.js
-import photographerFact from '../templates/photographerFact.js';
+import photographerFact from "../templates/photographerFact.js";
 
 // Import de la fonction MediaFact() du fichier mediaFact.js
-import MediaFact from '../templates/mediaFact.js';
+import MediaFact from "../templates/mediaFact.js";
 
 // Import des fonctions util du fichier utils.js
 import {
@@ -13,13 +12,13 @@ import {
   getIdFromUrl,
   getNameByID,
   sortMedia,
-} from '../utils/utils.js';
+} from "../utils/utils.js";
 
 // import de la fonction displayModal depuis le fichier contactFrom.js
-import displayModal from '../utils/contactForm.js';
+import displayModal from "../utils/contactForm.js";
 
 // import lightbox.js
-import LightBox from '../utils/lightbox.js';
+import LightBox from "../utils/lightbox.js";
 
 // Récupération des médias
 const fullmedias = datas.media;
@@ -27,43 +26,43 @@ const fullmedias = datas.media;
 // Récupération des données des éléments du DOM pour chaque photographe
 const photographerModel = photographerFact(photographer);
 const photographerPageDOM = photographerModel.getUserPageDOM();
-const Resume = document.querySelector('.resumeContainer');
-const selectButton = document.querySelector('.filterField_select');
-const selectList = document.querySelector('.filterField_select-list');
+const Resume = document.querySelector(".resumeContainer");
+const selectButton = document.querySelector(".filterField_select");
+const selectList = document.querySelector(".filterField_select-list");
 const selectItems = Array.from(
-  document.querySelectorAll('.filterField_select-list-item')
+  document.querySelectorAll(".filterField_select-list-item")
 );
 
 // Création de la base pour les médias
-const MediasContainer = document.createElement('section');
-MediasContainer.classList.add('MediasContainer');
-const sortLabel = document.getElementById('sortType');
-const nameShortened = getNameByID().split(' ')[0];
+const MediasContainer = document.createElement("section");
+MediasContainer.classList.add("MediasContainer");
+const sortLabel = document.getElementById("sortType");
+const nameShortened = getNameByID().split(" ")[0];
 // Récupération des éléments du DOM
-const photographHeader = document.querySelector('.photograph-header');
-const photographBody = document.querySelector('.photograph-body');
+const photographHeader = document.querySelector(".photograph-header");
+const photographBody = document.querySelector(".photograph-body");
 
 // Récupération des médias du photographe
 let Usermedias = fullmedias.filter((media) => media.photographerId == id);
-Usermedias = sortMedia('pop', Usermedias);
+Usermedias = sortMedia("pop", Usermedias);
 // Afficher la liste déroulante lorsque l'utilisateur clique sur le bouton
-selectButton.addEventListener('click', () => {
-    selectButton.setAttribute('aria-expanded', 'true');
-    selectList.classList.remove('hidden');
-  });
-  // Fermer la liste déroulante si l'utilisateur clique en dehors du bouton et de la liste
-  document.addEventListener('click', (event) => {
-    const isClickInside = selectButton.contains(event.target);
-    if (!isClickInside) {
-      selectButton.setAttribute('aria-expanded', 'false');
-      selectList.classList.add('hidden');
-    }
-  });
-  // déclare la variable du total de like
-  const TotalLikes = document.createElement('p');
-  TotalLikes.classList.add('TotalLikes');
-  TotalLikes.setAttribute('aria-label', 'Total des likes');
-  
+selectButton.addEventListener("click", () => {
+  selectButton.setAttribute("aria-expanded", "true");
+  selectList.classList.remove("hidden");
+});
+// Fermer la liste déroulante si l'utilisateur clique en dehors du bouton et de la liste
+document.addEventListener("click", (event) => {
+  const isClickInside = selectButton.contains(event.target);
+  if (!isClickInside) {
+    selectButton.setAttribute("aria-expanded", "false");
+    selectList.classList.add("hidden");
+  }
+});
+// déclare la variable du total de like
+const TotalLikes = document.createElement("p");
+TotalLikes.classList.add("TotalLikes");
+TotalLikes.setAttribute("aria-label", "Total des likes");
+
 // Ajout des données
 photographBody.appendChild(MediasContainer);
 Resume.appendChild(TotalLikes);
@@ -77,49 +76,49 @@ function displayData(photograph, medias) {
   const rawMedias = MediaFact(medias);
   const mediaModels = rawMedias.mediaElements;
   const Totalizer = rawMedias.TotalizeLikes;
-  TotalLikes.innerHTML = `${Totalizer} <i aria-label="likes" class="fas fa-heart"></i>`;
+  TotalLikes.innerHTML = `${Totalizer} <i aria-label="likes" class="fa-solid fa-heart"></i>`; 
   selectItems.forEach((item) => {
-    item.addEventListener('click', (event) => {
+    item.addEventListener("click", (event) => {
       event.stopPropagation();
       const selectedID = item.id;
       const selectedOption = item.textContent;
       // Tri des médias en fonction de l'option sélectionnée
       const sortedMedias = sortMedia(selectedID, medias);
       sortLabel.textContent = selectedOption;
-      selectList.classList.add('hidden');
-      selectButton.setAttribute('aria-expanded', 'false');
+      selectList.classList.add("hidden");
+      selectButton.setAttribute("aria-expanded", "false");
       // vide le container des médias et le slider de la lightbox
-      MediasContainer.innerHTML = '';
+      MediasContainer.innerHTML = "";
       // Utiliser les médias triés pour afficher les médias
       displayData(photograph, sortedMedias);
     });
   });
-   // Affichage des médias
-   if (medias) {
+  // Affichage des médias
+  if (medias) {
     for (let i = 0; i < mediaModels.length; i++) {
       const mediaModel = mediaModels[i];
       const mediamodelDOM = mediaModel.get_Media_Card_DOM(nameShortened);
       MediasContainer.appendChild(mediamodelDOM);
       mediamodelDOM.onclick = () => {
-        mediamodelDOM.classList.add('currentMedia');
+        mediamodelDOM.classList.add("currentMedia");
         const lightboxDOM = new LightBox(medias, nameShortened);
         lightboxDOM.open();
       };
       mediamodelDOM.onkeydown = (event) => {
-        if (event.key === 'Enter') {
-          mediamodelDOM.classList.add('currentMedia');
+        if (event.key === "Enter") {
+          mediamodelDOM.classList.add("currentMedia");
           const lightboxDOM = new LightBox(medias, nameShortened);
           lightboxDOM.open();
         }
-      }
+      };
     }
   }
   if (photograph) {
     photographHeader.appendChild(photographerPageDOM); // Affiche les données du photographe dans la page
     const contactbtn = document.querySelector(
-      '.photographContainer .contact_button'
+      ".photographContainer .contact_button"
     );
-    contactbtn.addEventListener('click', displayModal); // Affiche le formulaire de contact lors du click du bouton
+    contactbtn.addEventListener("click", displayModal); // Affiche le formulaire de contact lors du click du bouton
     Resume.appendChild(TotalLikes);
   }
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,16 +3,15 @@
  */
 
 // Récupération des éléments du DOM
-const BGmodal = document.querySelector('.contact_modal');
-const modal = document.querySelector('.modal');
-const modalTitle = document.getElementById('modalTitle');
-const modalTitleValidation = document.getElementById('modalTitle_Validate');
-const Body = document.getElementById('main-photographer');
-const CloseCrossBtn = document.querySelector('.close');
-const CloseModalBtn = document.getElementById('btn-close');
-const ValidateModal = document.querySelector('.modal_validate');
-const form = document.querySelector('form.reserve');
-
+const BGmodal = document.querySelector(".contact_modal");
+const modal = document.querySelector(".modal");
+const modalTitle = document.getElementById("modalTitle");
+const modalTitleValidation = document.getElementById("modalTitle_Validate");
+const Body = document.getElementById("main-photographer");
+const CloseCrossBtn = document.querySelector(".close");
+const CloseModalBtn = document.getElementById("btn-close");
+const ValidateModal = document.querySelector(".modal_validate");
+const form = document.querySelector("form.reserve");
 
 // Récupération des valeurs des éléments du formulaire
 const inputFirstName = document.forms.reserve.first;
@@ -25,55 +24,54 @@ const regexpEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 const regexpFirstName = /^[a-zA-Z\s]+$/;
 const regexpLastName = /^[a-zA-Z\s]+$/;
 
-
 // Liste des objets à vérifier + conditions + messages de retour en cas d'erreur
 const formfieldsObjects = [
   {
     formfield: inputFirstName,
     condition: () => !validateFirstName(),
-    message: ''
+    message: "",
   },
   {
     formfield: inputLastName,
     condition: () => !validateLastname(),
-    message: ''
+    message: "",
   },
   {
     formfield: inputEmail,
     condition: () => !validateEmail(),
-    message: 'Veuillez entrer une adresse e-mail valide.'
+    message: "",
   },
   {
     formfield: inputText,
     condition: () => !validateText(),
-    message: ''
-  }
+    message: "",
+  },
 ];
 
 // État de soumission du formulaire
 let alreadyValidate = false;
 // fermeture de la modale
-//CloseCrossBtn.addEventListener('click', closeForm);
+CloseCrossBtn.addEventListener("click", closeForm);
 if (CloseModalBtn) {
-  CloseModalBtn.addEventListener('click', closeForm);
+  CloseModalBtn.addEventListener("click", closeForm);
 }
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape' || e.key === 27) {
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" || e.key === 27) {
     closeForm();
   }
 
-  if (e.key === 'Enter' || e.key === 13) {
+  if (e.key === "Enter" || e.key === 13) {
     confirmValidation();
   }
 });
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
   if (e.target === modal) closeForm();
 });
 
 // Événements de validation du formulaire
-document.forms.reserve.addEventListener('submit', confirmValidation);
-document.forms.reserve.addEventListener('submit', (e) => {
+document.forms.reserve.addEventListener("submit", confirmValidation);
+document.forms.reserve.addEventListener("submit", (e) => {
   e.preventDefault();
   validate();
 });
@@ -83,41 +81,44 @@ document.forms.reserve.addEventListener('submit', (e) => {
  */
 function displayModal() {
   const contactbtn = document.querySelector(
-    '.photographContainer .contact_button'
+    ".photographContainer .contact_button"
   );
-  if (contactbtn){
-  contactbtn.setAttribute('aria-expanded', 'true');}
-    BGmodal.setAttribute('aria-hidden', 'false');
-  Body.setAttribute('aria-hidden', 'true');
-  
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' || e.key === 27) {
+  if (contactbtn) {
+    contactbtn.setAttribute("aria-expanded", "true");
+  }
+  BGmodal.setAttribute("aria-hidden", "false");
+  Body.setAttribute("aria-hidden", "true");
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" || e.key === 27) {
+      //numéro keypress pour échap
       closeForm();
     }
 
-    if (e.key === 'Enter' || e.key === 13) {
+    if (e.key === "Enter" || e.key === 13) {
+      //numéro keypress pour entrée
       confirmValidation();
     }
   });
-  document.addEventListener('click', (e) => {
-    if (e.target === modal) closeForm();
+  document.addEventListener("click", (e) => {
+    if (e.target === modal) closeForm(); // si la cible est la modal, fermer le formulaire
   });
 
   // Événements de validation du formulaire
-  document.forms.reserve.addEventListener('submit', confirmValidation);
-  document.forms.reserve.addEventListener('submit', (e) => {
-    e.preventDefault();
+  document.forms.reserve.addEventListener("submit", confirmValidation);
+  document.forms.reserve.addEventListener("submit", (e) => {
+    e.preventDefault(); // empêche de fermer la fenêtre sans récupérer les informations
     validate();
   });
 
   if (alreadyValidate) {
-    BGmodal.classList.add('visible');
+    BGmodal.classList.add("visible");
   } else {
-    BGmodal.classList.remove('hidden');
-    BGmodal.setAttribute('aria-hidden', 'false');
-    Body.setAttribute('aria-hidden', 'true');
+    BGmodal.classList.remove("hidden");
+    BGmodal.setAttribute("aria-hidden", "false");
+    Body.setAttribute("aria-hidden", "true");
     CloseCrossBtn.focus();
-    BGmodal.classList.add('visible');
+    BGmodal.classList.add("visible");
   }
 }
 
@@ -126,30 +127,32 @@ function displayModal() {
  */
 function closeForm() {
   setTimeout(() => {
-    BGmodal.classList.remove('visible');
-    BGmodal.classList.add('hidden');
-    document.removeEventListener('keydown', (e) => {
-      if (e.key === 'Escape' || e.key === 27) {
+    BGmodal.classList.remove("visible");
+    BGmodal.classList.add("hidden");
+    document.removeEventListener("keydown", (e) => {
+      if (e.key === "Escape" || e.key === 27) {
+        //numéro keypress pour échap
         closeForm();
       }
     });
 
-    document.removeEventListener('click', (e) => {
+    document.removeEventListener("click", (e) => {
       if (e.target === modal) closeForm();
     });
 
     // Événements de validation du formulaire
-    document.forms.reserve.removeEventListener('submit', confirmValidation);
-    document.forms.reserve.removeEventListener('submit', (e) => {
+    document.forms.reserve.removeEventListener("submit", confirmValidation);
+    document.forms.reserve.removeEventListener("submit", (e) => {
       e.preventDefault();
       validate();
     });
   }, 100);
   const contactbtn = document.querySelector(
-    '.photographContainer .contact_button'
+    ".photographContainer .contact_button"
   );
-  if (contactbtn){
-  contactbtn.setAttribute('aria-expanded', 'false');}
+  if (contactbtn) {
+    contactbtn.setAttribute("aria-expanded", "false");
+  }
 }
 
 /**
@@ -158,21 +161,21 @@ function closeForm() {
 function confirmValidation() {
   // const valide = true; pour tester le formulaire valide
   if (validate()) {
-    console.log('Formulaire valide');
-    console.log('firstName', inputFirstName.value);
-    console.log('lastName', inputLastName.value);
-    console.log('Email', inputEmail.value);
-    console.log('message', inputText.value);
-    modalTitle.classList.add('hidden');
-    modalTitle.classList.remove('visible');
-    form.classList.add('hidden');
-    form.classList.remove('visible');
-    modalTitleValidation.classList.remove('hidden');
-    modalTitleValidation.classList.add('visible');
-    CloseModalBtn.classList.remove('hidden');
-    CloseModalBtn.classList.add('visible');
-    ValidateModal.classList.remove('hidden');
-    ValidateModal.classList.add('visible');
+    console.log("Formulaire valide");
+    console.log("firstName", inputFirstName.value);
+    console.log("lastName", inputLastName.value);
+    console.log("Email", inputEmail.value);
+    console.log("message", inputText.value);
+    modalTitle.classList.add("hidden");
+    modalTitle.classList.remove("visible");
+    form.classList.add("hidden");
+    form.classList.remove("visible");
+    modalTitleValidation.classList.remove("hidden");
+    modalTitleValidation.classList.add("visible");
+    CloseModalBtn.classList.remove("hidden");
+    CloseModalBtn.classList.add("visible");
+    ValidateModal.classList.remove("hidden");
+    ValidateModal.classList.add("visible");
     CloseModalBtn.focus();
 
     alreadyValidate = true;
@@ -184,14 +187,17 @@ function confirmValidation() {
  * @return {boolean} - Retourne vrai si le prénom est valide
  */
 function validateFirstName() {
-  if (inputFirstName.value.trim().length < 2) {
+  if (
+    inputFirstName.value.trim().length < 2 || // si nombre de caractères inférieurs à 2
+    inputFirstName.value.trim() === "" // si champ vide
+  ) {
     formfieldsObjects[0].message =
-      'Le champ doit comporter un minimum de 2 caractères alphabétiques.';
+      "Le champ doit comporter un minimum de 2 caractères alphabétiques.";
     return false;
   }
   if (!regexpFirstName.test(inputFirstName.value.trim())) {
     formfieldsObjects[0].message =
-      'Le champ doit comporter seulement des caractères alphabétiques.';
+      "Le champ doit comporter seulement des caractères alphabétiques.";
     return false;
   }
   return true;
@@ -203,16 +209,16 @@ function validateFirstName() {
  */
 function validateLastname() {
   if (
-    inputLastName.value.trim().length < 2 ||
-    inputLastName.value.trim() === ''
+    inputLastName.value.trim().length < 2 || // si nombre de caractères inférieurs à 2
+    inputLastName.value.trim() === "" // si champ vide
   ) {
     formfieldsObjects[1].message =
-      'Le champ doit comporter un minimum de 2 caractères alphabétiques.';
+      "Le champ doit comporter un minimum de 2 caractères alphabétiques.";
     return false;
   }
   if (!regexpLastName.test(inputLastName.value.trim())) {
     formfieldsObjects[1].message =
-      'Le champ doit comporter seulement des caractères alphabétiques.';
+      "Le champ doit comporter seulement des caractères alphabétiques.";
     return false;
   }
   return true;
@@ -224,7 +230,7 @@ function validateLastname() {
  */
 function validateEmail() {
   if (!regexpEmail.test(inputEmail.value.trim())) {
-    formfieldsObjects[2].message = 'Veuillez entrer une adresse mail valide.';
+    formfieldsObjects[2].message = "Veuillez entrer une adresse mail valide.";
     return false;
   }
   return true;
@@ -236,7 +242,13 @@ function validateEmail() {
  */
 function validateText() {
   if (inputText.value.trim().length < 50) {
-    formfieldsObjects[3].message = 'Veuillez entrer au minimum 50 caractères.';
+    //minimum 50 caractères
+    formfieldsObjects[3].message = "Veuillez entrer au minimum 50 caractères.";
+    return false;
+  }
+  if (inputText.value.trim().length > 20) {
+    //minimum 50 caractères
+    formfieldsObjects[3].message = "Veuillez entrer au maximum 200 caractères.";
     return false;
   }
   return true;
@@ -253,25 +265,25 @@ function validate() {
     const { message } = formfieldsObjects[i];
     if (condition) {
       formfieldsObjects[i].formfield.parentElement.setAttribute(
-        'data-error',
+        "data-error",
         message
       );
       formfieldsObjects[i].formfield.parentElement.setAttribute(
-        'data-error-visible',
-        'true'
+        "data-error-visible",
+        "true"
       );
-      formfieldsObjects[i].formfield.parentElement.classList.add('error');
+      formfieldsObjects[i].formfield.parentElement.classList.add("error");
       formfieldsObjects[i].formfield.focus();
       formIsTrue = false;
     } else {
       formfieldsObjects[i].formfield.parentElement.removeAttribute(
-        'data-error'
+        "data-error"
       );
       formfieldsObjects[i].formfield.parentElement.setAttribute(
-        'data-error-visible',
-        'false'
+        "data-error-visible",
+        "false"
       );
-      formfieldsObjects[i].formfield.parentElement.classList.remove('error');
+      formfieldsObjects[i].formfield.parentElement.classList.remove("error");
     }
   }
   return formIsTrue;
